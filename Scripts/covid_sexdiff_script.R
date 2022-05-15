f8aed772fd0a05cca960f98fcacd8a9cc003bfe6
Pietro Violo <pietro.violo@umontreal.ca>
2022-05-15 12:33:34 -0400

    Header, library and data

--- Scripts/covid_sexdiff_script.R
+++ Scripts/covid_sexdiff_script.R
@@ -1,5 +1,16 @@
-# Libraries
+#------------------------------------------------------------------------------#
+# Name : covid_sexdiff_script.R                                                #
+# Description : Standardize and compare mortality rates for COVID-19           #
+# and other diseases, as well as calculating and plotting their male-to-female #
+# ratios                                                                       #
+# Author : Pietro Violo                                                        #
+# Date : May 15 2022                                                           #
+#------------------------------------------------------------------------------#
 
-library(tidyverse)
+# Libraries
 library(ggthemes)
 library(mortalitySmooth)
+
+# Import data
+covid_data <- read.csv("./Data/Provisional_COVID-19_Deaths_by_Sex_and_Age.csv")
+
